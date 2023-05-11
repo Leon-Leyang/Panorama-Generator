519885b7f24c2a416d3f015574ec08c82113c6be
Leon-Leyang <scylh6@nottingham.edu.cn>
2023-05-11 16:04:44 +0800

    Clean up the code

--- main.py
+++ main.py
@@ -44,5 +44,5 @@ print('Generating panorama...')
 num_levels = 3
 result_path = os.path.join('result', video_path.split('/')[-1].split('.')[0] + '.jpg')
 s_time = time.time()
-panorama = PanoramaGenerator.gen_panorama(warped_frames, num_levels, result_path.replace('garden_stable', 'garden_stable_3'))
+panorama = PanoramaGenerator.gen_panorama(warped_frames, num_levels, result_path)
 print(f'Done! Saved the panorama in {result_path}. Take {time.time() - s_time:.2f}s.\n')
